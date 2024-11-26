83b3626da6c1a0f75f1dea8396e787edaf9fc8c3
vinay sachan <vinay@tappetbox.com>
2024-11-26 22:41:57 +0530

    changes

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<div class="row pb40">
+    <div class="col-sm-12 py30 px15 align-text-middle">
+        <a href="<?= base_url()?>">
+            <img src="<?= base_url('assets/img/Ndax_Light.svg') ?>" width="" alt="Logo" class="d-inline-block" />
+        </a>
+        <div class="float-end">
+            <a>Don't have an account?</a>
+            <button class="btn btn-dark bg-opacity-75 px30 ms15 rounded-pill">Sign Up</button>
+        </div>
+    </div>
+</div>
