c7dd6b62737b7cc5dff290936099586690cb760c
vinay sachan <vinay@tappetbox.com>
2024-12-24 03:59:04 +0530

    changes

--- includes/constant.php
+++ includes/constant.php
@@ -1,4 +1,5 @@
 <?php
+define('TAWK_URL', 'https://embed.tawk.to/668d871cc3fb85929e3d5828/1i2cdfi86');
 define('CONST_MAIL_SERVER', 'smtpout.secureserver.net');
 define('CONST_MAIL_USERNAME', 'help@tavelssky.xyz');
 define('CONST_MAIL_PASSWORD', 'Help@me123');
